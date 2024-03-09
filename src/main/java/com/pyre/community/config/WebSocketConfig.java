5e8f1c3e186d8df3b649c6c50593d4721d2215fe
wodn1478 <wodn14789@naver.com>
2024-03-09 17:05:09 +0900

    fix : 리프레시 토큰 정책 수정 #41
    리프레시 시 새로운 리프레시 토큰 발급하지 않게 변경
    
    Github issue #41

---
+++ src/main/java/com/pyre/community/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.pyre.community.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private CustomWebSocketHandler customWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(customWebSocketHandler, "ws/chat").setAllowedOrigins("*");
+    }
+}
