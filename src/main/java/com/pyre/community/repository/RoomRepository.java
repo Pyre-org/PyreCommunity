28c1a3f31644bc6311fe61bbf82e91f8079b087b
wodn1478 <wodn14789@naver.com>
2024-03-19 22:41:27 +0900

    fix : 룸 검색 시 키워드가 포함되지 않던 문제 해결 #71

--- src/main/java/com/pyre/community/repository/RoomRepository.java
+++ src/main/java/com/pyre/community/repository/RoomRepository.java
@@ -14,9 +14,9 @@ import java.util.UUID;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, UUID> {
-    List<Room> findAllByChannelAndTypeAndTitleContainingOrderByTitle(Channel channel, RoomType type, String title);
+    Page<Room> findAllByChannelAndTypeAndTitleContaining(Channel channel, RoomType type, String title, Pageable pageable);
     Room findByChannelAndType(Channel channel, RoomType type);
     Boolean existsByChannelAndTitle(Channel channel, String title);
-    @Query("SELECT r FROM Room r WHERE (r.title LIKE %?1% AND r.type = ?2) OR r in (SELECT re.room FROM RoomEndUser re WHERE re.userId = ?3 AND re.isDeleted = false)")
+    @Query("SELECT r FROM Room r WHERE (r.title LIKE %?1% AND r.type = ?2) OR r in (SELECT re.room FROM RoomEndUser re WHERE re.userId = ?3 AND re.isDeleted = false AND (re.room.title LIKE %?1%)) ")
     Page<Room> findAllByTitleSearch(String title, RoomType type, UUID userId, Pageable pageable);
 }
